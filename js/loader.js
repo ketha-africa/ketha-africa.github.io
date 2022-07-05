116ea0102cb271b7ccd8ce7f8e7a593213f61260
Maria2020-beep <74241652+Maria2020-beep@users.noreply.github.com>
2022-07-05 11:35:05 +0300

    Loader

---
+++ js/loader.js
@@ -0,0 +1,18 @@
+var imgs = document.images,
+    len = imgs.length,
+    counter = 0;
+
+[].forEach.call( imgs, function( img ) {
+    if(img.complete)
+      incrementCounter();
+    else
+      img.addEventListener( 'load', incrementCounter, false );
+} );
+
+function incrementCounter() {
+    counter++;
+    if ( counter === len ) {
+        document.getElementById("loader").style.display = "none";
+        console.log("All images are loaded");
+    }
+}
